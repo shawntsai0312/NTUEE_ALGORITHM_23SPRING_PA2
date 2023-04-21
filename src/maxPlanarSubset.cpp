2bda4c955b6af36c313eaf3825a8f0efd98831ce
shawntsai0312 <shawntsai0312@gmail.com>
2023-04-22 00:45:50 +0800

    add counter

--- src/maxPlanarSubset.cpp
+++ src/maxPlanarSubset.cpp
@@ -8,42 +8,50 @@ using namespace std;
 // mps_tool.h
 int **Matrix2D(int, int);
 int **MPS(int *, int);
-void MPS_result(int **, int *, queue<int>&, int, int);
+void MPS_result(int **, int *, queue<int> &, int, int);
 // mps_tool.cpp
-int **Matrix2D(int n, int m)//declare nxm matrix
+int **Matrix2D(int n, int m) // declare nxm matrix
 {
-    int** Mtx = new int*[n];
-    for(int i=0; i<n; i++)
+    int **Mtx = new int *[n];
+    for (int i = 0; i < n; i++)
     {
         Mtx[i] = new int[m];
     }
     return Mtx;
 }
+
+long long counter1 = 0;
+long long counter2 = 0;
+long long counter3 = 0;
+
 int **MPS(int *C, int n)
 {
     int **M = Matrix2D(n, n);
     int **A = Matrix2D(n, n);
-    for (int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         M[i][i] = 0;
     }
-    for (int i=n-2; i>=0; i--)
+    for (int i = n - 2; i >= 0; i--)
     {
-        for (int j=i+1; j<n; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if (C[i]<i || C[i]>j)
+            if (C[i] < i || C[i] > j)
             {
-                M[i][j] = M[i+1][j];
+                counter1++;
+                M[i][j] = M[i + 1][j];
             }
-            else if (C[i]==j)
+            else if (C[i] == j)
             {
-                M[i][j] = M[i+1][j-1] + 1;
+                counter2++;
+                M[i][j] = M[i + 1][j - 1] + 1;
                 A[i][j] = 1;
             }
             else
             {
-                int q1 = M[i+1][C[i]-1] + M[C[i]+1][j] + 1;
-                int q2 = M[i+1][j];
+                counter3++;
+                int q1 = M[i + 1][C[i] - 1] + M[C[i] + 1][j] + 1;
+                int q2 = M[i + 1][j];
                 if (q1 >= q2)
                 {
                     M[i][j] = q1;
@@ -58,42 +66,42 @@ int **MPS(int *C, int n)
     }
     return A;
 }
-void MPS_result(int **A, int *C, queue<int>& R, int p, int r)
+void MPS_result(int **A, int *C, queue<int> &R, int p, int r)
 {
-    if(r<=p)
+    if (r <= p)
         return;
-    
-    switch(A[p][r])
+
+    switch (A[p][r])
     {
-        case 1:
-            R.push(p);
-            MPS_result(A, C, R, p+1, r-1);
-            break;
-        case 2:
-            R.push(p);
-            MPS_result(A, C, R, p+1, C[p]-1);
-            MPS_result(A, C, R, C[p]+1, r);
-            break;
-        default:
-            MPS_result(A, C, R, p+1, r);
-            break;
+    case 1:
+        R.push(p);
+        MPS_result(A, C, R, p + 1, r - 1);
+        break;
+    case 2:
+        R.push(p);
+        MPS_result(A, C, R, p + 1, C[p] - 1);
+        MPS_result(A, C, R, C[p] + 1, r);
+        break;
+    default:
+        MPS_result(A, C, R, p + 1, r);
+        break;
     }
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    //read input
+    // read input
     char buffer[200];
     fstream fin(argv[1]);
     fstream fout;
-    fout.open(argv[2],ios::out);
+    fout.open(argv[2], ios::out);
     int pts;
     fin >> pts;
-    fin.getline(buffer,200);
+    fin.getline(buffer, 200);
     int num1, num2;
 
     int *Set = new int[pts];
-    for (int i=0; i<pts; i++)
+    for (int i = 0; i < pts; i++)
     {
         Set[i] = -1;
     }
@@ -103,23 +111,26 @@ int main(int argc, char* argv[])
         Set[num1] = num2;
         Set[num2] = num1;
     }
-    //find answer
+    // find answer
     int **Ans = Matrix2D(pts, pts);
     Ans = MPS(Set, pts);
     queue<int> Result;
-    MPS_result(Ans, Set, Result, 0, pts-1);
+    MPS_result(Ans, Set, Result, 0, pts - 1);
 
-    //result
+    // result
     int resultSize = Result.size();
-    fout<<resultSize<<endl;
-    for (int i=0;i<resultSize;i++)
+    fout << resultSize << endl;
+    for (int i = 0; i < resultSize; i++)
     {
-        fout<<Result.front()<<" "<<Set[Result.front()]<<endl;
+        fout << Result.front() << " " << Set[Result.front()] << endl;
         Result.pop();
     }
     fin.close();
     fout.close();
     clock_t totalTime = clock();
-    cout<<"total time: "<<(float)totalTime/CLOCKS_PER_SEC<<endl;
+    cout << "total time: " << (float)totalTime / CLOCKS_PER_SEC << endl;
+    cout << "counter 1 " << counter1 << endl;
+    cout << "counter 2 " << counter2 << endl;
+    cout << "counter 3 " << counter3 << endl;
     return 0;
 }
